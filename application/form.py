56201dc8d637b6e53902b595ae640286a5316765
Taen <taen.heizo8@gmail.com>
2020-03-30 22:05:55 -0400

    user registration form and validation

--- application/form.py
+++ application/form.py
@@ -1,11 +1,12 @@
 from wtforms import Form, SubmitField, StringField, PasswordField, validators
 
 
+# form for user registration
 class UserRegistrationForm(Form):
     email = StringField('Email Address', [
         validators.Email(message="Not a valid email address"), validators.DataRequired()])
     password = PasswordField('Password', [
-        validators.DataRequired(message="Please enter a password.")])
+        validators.DataRequired(message="Please enter a password."), validators.Length(min=6)])
     confirm_password = PasswordField('Confirm Password', [
-        validators.EqualTo(password, message="Passwords must match.")])
+        validators.EqualTo('password', message="Passwords must match.")])
     submit = SubmitField('Submit')
--- application/views/user.py
+++ application/views/user.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, redirect, url_for
 
+from application.form import UserRegistrationForm
 from application.navigation import top_level_nav
 
 user = Blueprint('user', __name__, template_folder='templates', static_folder='static')
@@ -7,6 +8,16 @@ user = Blueprint('user', __name__, template_folder='templates', static_folder='s
 
 @user.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == 'GET':
-        return render_template('user/signup.html', title="sign up",
-                               description="create new account", nav=top_level_nav())
+    signup_form = UserRegistrationForm(request.form)
+    if request.method == 'POST':
+        if signup_form.validate():
+            print("success")
+            return redirect(url_for('user.login'))
+    return render_template('user/signup.html', title="sign up",
+                           description="create new account", nav=top_level_nav(), form=signup_form)
+
+
+@user.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('user/login.html', title="login",
+                           description="login", nav=top_level_nav())

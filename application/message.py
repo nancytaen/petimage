96b5228b6d61938e44c46c2ca1fa56c1759a4fda
EC2 Default User <ec2-user@ip-172-31-19-83.us-east-2.compute.internal>
2020-04-10 17:04:06 +0000

    added a class of constant messages

---
+++ application/message.py
@@ -0,0 +1,6 @@
+# user related messages
+class UserMessage:
+    SIGNUP_SUCCESS = "Account has been successfully created."
+    LOGIN_SUCCESS = 0
+    USER_EXISTS = " is already used."
+    EMAIL_PASSWORD_NOT_MATCH = " or password is wrong."

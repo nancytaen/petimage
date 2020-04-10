567f484a43b9d562dafad1558e5177a4315e9675
EC2 Default User <ec2-user@ip-172-31-19-83.us-east-2.compute.internal>
2020-04-10 15:48:27 +0000

    modified form label and description

--- application/form.py
+++ application/form.py
@@ -3,11 +3,11 @@ from wtforms import Form, SubmitField, StringField, PasswordField, validators
 
 # form for user registration
 class UserRegistrationForm(Form):
-    email = StringField('Email Address', [
+    email = StringField("email", description="Email Address", validators=[
         validators.Email(message="Not a valid email address"), validators.DataRequired()])
-    password = PasswordField('Password', [
+    password = PasswordField('password', description="Password", validators=[
         validators.DataRequired(message="Please enter a password."), validators.Length(min=6)])
-    confirm_password = PasswordField('Confirm Password', [
+    confirm_password = PasswordField('confirm_password', description='Confirm Password', validators=[
         validators.EqualTo('password', message="Passwords must match.")])
     submit = SubmitField('Create Account')
 

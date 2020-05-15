4027d227cb15a45784f901c08a8ebe3b21c845b9
nancytaen <taen.heizo8@gmail.com>
2020-05-15 12:47:08 -0400

    separate js like function into a different file

---
+++ application/static/js/post_action.js
@@ -0,0 +1,15 @@
+// like post and toggle button, change number
+function LikePost(post_id){
+  $.get('/post/like/' + post_id, function(data){
+    if (data.status === 0){
+      const button = $('#LikeButton' + post_id);
+      const num = $('#NumLikes' + post_id);
+      if (button.hasClass("btn-danger")){
+        num.html(parseInt(num.html(), 10) - 1);
+      } else {
+        num.html(parseInt(num.html(), 10) + 1);
+      }
+      button.toggleClass("btn-danger btn-outline-danger");
+    }
+  })
+}
